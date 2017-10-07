3f3c8979007ac4762b4cc96064f463b1d1204028
Dicky Ho <me@ckho.info>
2017-10-07 14:49:44 +0200

    Add Name and Image Fields to Users Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007121231) do
+ActiveRecord::Schema.define(version: 20171007124640) do
 
   create_table "job_posts", force: :cascade do |t|
     t.string   "job"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20171007121231) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
