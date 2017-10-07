e180a44076465d6e41c703ac92ba2402649189b7
Dicky Ho <me@ckho.info>
2017-10-07 14:40:24 +0200

    Login Part with Linkedin SSO

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
